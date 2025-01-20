f6d36da553d2a0654c3e0c9f24129eae47ab3316
Luana Lima <luanalima.2006@hotmail.com>
2025-01-20 14:48:54 -0300

    feat: finish resolving js problems

--- exercicios-de-js/script.js
+++ exercicios-de-js/script.js
@@ -12,3 +12,24 @@ let boletoPago = false;
 boletoPago
   ? console.log("O boleto está pago")
   : console.log("O boleto não está pago");
+
+// q3
+let resposta = confirm(
+  "Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?"
+);
+
+if (resposta) {
+  welcomeMessage.innerHTML = "Ótimo! Nós temos as melhores camas de toda a região!";
+} else {
+  welcomeMessage.innerHTML = "Que pena! Você parecia ser uma pessoa mais legal";
+}
+
+//q4
+for (let i = 0; i < 10; i++) {
+  console.log("O número atual é o", i);
+}
+
+// q5
+for (let i = 0; i < 10; i++) {
+  console.log(`O dobro do número ${i} é ${i * 2}`);
+}
